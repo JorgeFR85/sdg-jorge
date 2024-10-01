f278563496ffc1f3c507f7e579cbf3568bffc10e
Jorge G.C <jorgonzalez@gmail.com>
2024-10-01 13:30:04 +0200

    f

--- main.tf
+++ main.tf
@@ -1,26 +1,24 @@
-# Este recurso `null_resource` ejecutará comandos para instalar Kubernetes en una máquina Debian
 resource "null_resource" "install_kubernetes" {
-  
-  # Utilizamos el provisioner `local-exec` para ejecutar comandos en la máquina local
   provisioner "local-exec" {
-
-    # El bloque de comandos que ejecutaremos:
-    command = <<-EOT
+    command = <<EOT
       # Actualizar los índices de los paquetes del sistema
       sudo apt-get update &&
       
       # Instalar dependencias necesarias para Kubernetes
       sudo apt-get install -y apt-transport-https ca-certificates curl gnupg &&
-      
+
       # Crear el directorio de keyrings si no existe
       sudo mkdir -p /etc/apt/keyrings &&
-      
+
+      # Eliminar claves corruptas si las hay
+      sudo rm /etc/apt/keyrings/kubernetes-apt-keyring.gpg &&
+
       # Descargar la llave pública firmada del repositorio de Kubernetes
-      curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-      
+      curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg &&
+
       # Agregar el repositorio de Kubernetes a la lista de fuentes
       echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list &&
-      
+
       # Volver a actualizar los índices de los paquetes tras añadir el repositorio
       sudo apt-get update
     EOT
