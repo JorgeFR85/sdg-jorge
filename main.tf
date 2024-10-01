6ae4fde383278dca457cbce379da04e8408e9b6d
Jorge G.C <jorgonzalez@gmail.com>
2024-10-01 13:19:32 +0200

    g

--- main.tf
+++ main.tf
@@ -1,33 +1,28 @@
+# Este recurso `null_resource` ejecutará comandos para instalar Kubernetes en una máquina Debian
 resource "null_resource" "install_kubernetes" {
+  
+  # Utilizamos el provisioner `local-exec` para ejecutar comandos en la máquina local
   provisioner "local-exec" {
-    command = <<EOT
-      # Actualizar los repositorios e instalar dependencias
-      sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
 
-      # Descargar la clave GPG y agregar el repositorio correcto para Debian
-      sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-      echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-      # Actualizar los repositorios e instalar kubeadm, kubelet y kubectl
-      sudo apt-get update && sudo apt-get install -y kubelet kubeadm kubectl
-
-      # Marcar kubeadm, kubelet y kubectl para que no se actualicen automáticamente
-      sudo apt-mark hold kubelet kubeadm kubectl
-
-      # Inicializar el clúster de Kubernetes con kubeadm
-      sudo kubeadm init --apiserver-advertise-address=192.168.2.26 --pod-network-cidr=192.168.0.0/16
-
-      # Configurar kubectl para el usuario actual
-      mkdir -p $HOME/.kube
-      sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-      sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-      # Instalar la red de Calico para el clúster de Kubernetes
-      kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+    # El bloque de comandos que ejecutaremos:
+    command = <<-EOT
+      # Actualizar los índices de los paquetes del sistema
+      sudo apt-get update &&
+      
+      # Instalar dependencias necesarias para Kubernetes
+      sudo apt-get install -y apt-transport-https ca-certificates curl gnupg &&
+      
+      # Crear el directorio de keyrings si no existe
+      sudo mkdir -p /etc/apt/keyrings &&
+      
+      # Descargar la llave pública firmada del repositorio de Kubernetes
+      curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg &&
+      
+      # Agregar el repositorio de Kubernetes a la lista de fuentes
+      echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list &&
+      
+      # Volver a actualizar los índices de los paquetes tras añadir el repositorio
+      sudo apt-get update
     EOT
-  }
-
-  triggers = {
-    always_run = "${timestamp()}"
   }
 }
