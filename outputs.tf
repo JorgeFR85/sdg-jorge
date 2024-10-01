89fd13070b315a21ca1311f4d4def52761761bb5
Jorge G.C <jorgonzalez@gmail.com>
2024-10-01 14:32:49 +0200

    output

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,3 @@
-# Output para obtener la configuración de Kubernetes
-output "kubeconfig" {
-  description = "Archivo de configuración de kubeconfig"
-  value       = module.kubernetes.kubeconfig
-  sensitive   = true
-}
-
 # Output para mostrar la IP del nodo master de Kubernetes
 output "master_ip" {
   description = "IP del nodo master de Kubernetes"
